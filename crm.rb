64691e6d68948d0c75e05c944f6b248887c82afa
eiji <eijimine@gmail.com>
2017-07-14 13:32:39 -0400

    Working on EDIT form

--- crm.rb
+++ crm.rb
@@ -29,6 +29,25 @@ get ('/contacts/:id') do
  end
 end
 
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+    redirect to('/contacts')
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 after do
   ActiveRecord::Base.connection.close
 end
